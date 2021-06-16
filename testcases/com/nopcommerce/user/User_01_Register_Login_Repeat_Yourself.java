c7a1b17f609991576c756c46dba22f892974370d
ThangLV <thang.le.fc@gmail.com>
2021-06-16 18:47:09 +0700

    update

--- testcases/com/nopcommerce/user/User_01_Register_Login_Repeat_Yourself.java
+++ testcases/com/nopcommerce/user/User_01_Register_Login_Repeat_Yourself.java
@@ -1,35 +1,92 @@
 package com.nopcommerce.user;
 
+import java.util.Random;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class User_01_Register_Login_Repeat_Yourself {
 	WebDriver driver;
+	Select select;
 	String projectPath = System.getProperty("user.dir");
+	String firstName, lastName, day, month, year, emailAddress, companyName, password;
 	
 	@BeforeClass
 	public void beforeClass() {
 		System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://demo.nopcommerce.com/");
+		
+		firstName = "Automation";
+		lastName = "FC";
+		day = "10";
+		month = "May";
+		year = "1999";
+		emailAddress = "automation" + getRandomNumber() + "@mailinator.com";
+		companyName = "Automation FC";
+		password = "123456";
 	}
 	
 	@Test
-	public void TC_01() {
+	public void TC_01_Register() {
+		driver.findElement(By.className("ico-register")).click();
 		
+		driver.findElement(By.id("gender-male")).click();
+		driver.findElement(By.id("FirstName")).sendKeys(firstName);
+		driver.findElement(By.id("LastName")).sendKeys(lastName);
+		
+		select = new Select(driver.findElement(By.name("DateOfBirthDay")));
+		select.selectByVisibleText(day);
+		
+		select = new Select(driver.findElement(By.name("DateOfBirthMonth")));
+		select.selectByVisibleText(month);
+		
+		select = new Select(driver.findElement(By.name("DateOfBirthYear")));
+		select.selectByVisibleText(year);
+		
+		driver.findElement(By.id("Email")).sendKeys(emailAddress);
+		driver.findElement(By.id("Company")).sendKeys(companyName);
+		
+		if(!driver.findElement(By.id("Newsletter")).isSelected()) {
+			driver.findElement(By.id("Newsletter")).click();
+		}
+		
+		driver.findElement(By.id("Password")).sendKeys(password);
+		driver.findElement(By.id("ConfirmPassword")).sendKeys(password);
+		
+		driver.findElement(By.id("register-button")).click();
+		
+		Assert.assertEquals(driver.findElement(By.cssSelector(".result")).getText(), "Your registration completed");
+		
+		driver.findElement(By.className("ico-logout")).click();
 	}
 	
 	@Test
-	public void TC_02() {
+	public void TC_02_Login() {
+		driver.findElement(By.className("ico-login")).click();
 		
+		driver.findElement(By.id("Email")).sendKeys(emailAddress);
+		driver.findElement(By.id("Password")).sendKeys(password);
+		driver.findElement(By.cssSelector(".login-button")).click();
+		
+		Assert.assertTrue(driver.findElement(By.cssSelector(".ico-account")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.className("ico-logout")).isDisplayed());
 	}
 	
 	@Test
-	public void TC_03() {
-		
+	public void TC_03_My_Account() {
+		driver.findElement(By.cssSelector(cssSelector))
+	}
+	
+	public int getRandomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
 	}
 	
 	@AfterClass
