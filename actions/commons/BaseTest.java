f143d1f49ec8e256b3a8006185a548df9480de48
VanThangLe <thang.le.fc@gmail.com>
2021-07-04 13:58:12 +0700

    commit topic 43

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -5,25 +5,45 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
+	
 	private WebDriver driver;
 	private String projectPath = System.getProperty("user.dir");
 
 	protected WebDriver getBrowserDriver(String browserName, String url) {
-		if(browserName.equalsIgnoreCase("firefox")) {
+		Browser browser = Browser.valueOf(browserName.toUpperCase());
+		
+		switch (browser) {
+		case FIREFOX:
+			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
-		} else if(browserName.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver.exe");
+			break;
+		case CHROME:
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-		} else if(browserName.equalsIgnoreCase("edge")) {
-			System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver.exe");
+			break;
+		case EDGE:
+			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
-		} else {
+			break;
+		case COCCOC:
+			//Chrome driver trừ 5 verison
+			WebDriverManager.chromedriver().driverVersion("89.0.4389.23").setup();
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("C:\\Users\\thang\\AppData\\Local\\CocCoc\\Browser\\Application\\browser.exe");
+			driver = new ChromeDriver(options);
+			break;
+		default:
 			throw new RuntimeException("Browser name is not correct!");
 		}
+		
 		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		return driver;
