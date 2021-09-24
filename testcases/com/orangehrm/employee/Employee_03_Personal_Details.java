de82454324772f28c28e5ed02fe08ab1a0f64ea7
ThangLV <thang.le.fc@gmail.com>
2021-09-24 17:47:53 +0700

    commit generate testcase

---
+++ testcases/com/orangehrm/employee/Employee_03_Personal_Details.java
@@ -0,0 +1,32 @@
+package com.orangehrm.employee;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+
+public class Employee_03_Personal_Details extends BaseTest {
+	WebDriver driver;
+	
+	@Parameters({"browser", "url"})
+	@BeforeClass
+	public void beforeClass(String browserName, String appUrl) {
+		log.info("Pre-condition: Open browser '" + browserName + "'and navigate to '" + appUrl + "'");
+		driver = getBrowserDriver(browserName, appUrl);
+	}
+	
+	@Test
+	public void TC_01_Add_New_Employee() {
+		
+	}
+	
+	@Parameters({"browser"})
+	@AfterClass(alwaysRun = true)
+	public void cleanBrowser(String browserName) {
+		log.info("Post-condition: Close browser '" + browserName + "'");
+		cleanDriverInstance();
+	}
+}
