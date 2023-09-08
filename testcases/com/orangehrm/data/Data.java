cbf8653d0c6a4bbc726cf485c665958cc52f081d
VanThangLe <thang.le.fc@gmail.com>
2023-09-08 14:21:04 +0700

    update

---
+++ testcases/com/orangehrm/data/Data.java
@@ -0,0 +1,39 @@
+package com.orangehrm.data;
+
+public class Data {
+	public static class Employee_01_Add_New_Employee {
+		public static final String a = "";
+	}
+	
+	public class Employee_03_Personal_Details {
+		
+	}
+
+	public class Employee_04_Contact_Details {
+	
+	}
+
+	public class Employee_05_Emergency_Details {
+	
+	}
+	
+	public class Employee_06_Assigned_Dependents {
+		
+	}
+	
+	public class Employee_07_Edit_View_Job {
+		
+	}
+
+	public class Employee_08_Edit_View_Salary {
+	
+	}
+
+	public class Employee_09_Edit_View_Tax {
+	
+	}
+	
+	public class Employee_11_Search_Employee {
+		
+	}
+}
